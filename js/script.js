9209ed2613a4f486d327e8272e90c9f9f730b15b
MaxPower55 <75630969+MaxPower55@users.noreply.github.com>
2022-02-14 14:46:35 +0600

    переделал скрипт просмотра картинок на весь экран

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 var imgs = document.querySelectorAll('.item__img');
 for (var i = 0;i < imgs.length;i++) {
     targetImg = imgs[i];
-    targetImg.addEventListener('click', function() {
-        this.classList.toggle('item__img-fs')
-    })
-}
+    targetImg.addEventListener('click', function(e) {
+        imgOriginalSrc = this.getAttribute('src');
+
+        var bodyPage = document.getElementById('body');
+        var imgWrapper = document.createElement('div');
+        var imgSource = document.createElement('img');
+        var imgClose = document.createElement('span');
+
+        bodyPage.appendChild(imgWrapper);
+        imgWrapper.appendChild(imgSource);
+        imgWrapper.appendChild(imgClose);
+
+        imgWrapper.className = 'image-full-screen-viewer';
+        imgSource.className = 'image-source';
+        imgClose.className = 'image-close';
+        imgClose.textContent = 'x';
+        imgSource.setAttribute('src', imgOriginalSrc);     
+        
+        imgClose.addEventListener('click', function() {
+            bodyPage.removeChild(imgWrapper);
+        });
+    });
+};
