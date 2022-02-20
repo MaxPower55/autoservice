1453551b2fb32c8da65c7745431ab9e8ae1d3196
MaxPower55 <andrewrubanov@gmail.com>
2022-02-20 16:25:03 +0300

    встроил яндекс-карту

--- js/script.js
+++ js/script.js
@@ -24,3 +24,27 @@ for (var i = 0;i < imgs.length;i++) {
         });
     });
 };
+
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.028486732905726,73.29739090599446],
+            zoom: 17
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+        myMap.controls.remove('geolocationControl');
+        myMap.controls.remove('searchControl');
+        myMap.controls.remove('trafficControl');
+        myMap.controls.remove('typeSelector');
+        myMap.controls.remove('fullscreenControl');
+        myMap.controls.remove('rulerControl');
+        let myPlacemark = new ymaps.Placemark([55.028486732905726,73.29739090599446], {}, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/where/marker.png',
+            iconImageSize: [40, 40],
+            iconImageOffset: [-20, -40]
+        });
+    myMap.geoObjects
+        .add(myPlacemark);
+        // .add(myPlacemarkWithContent);
+});
